bab53b836a32dea330a5f8c39e0fbb86a57f1b31
Nestor Cañás <ncanas@openmailbox.org>
2017-09-11 23:31:34 -0600

    juegos con las clases de hola mundo

---
+++ JAVA/Java Class/GUI/holamundogui.java
@@ -0,0 +1,11 @@
+/**
+ * holamundogui
+ */
+import javax.swing.*;
+
+public class holamundogui {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola Mundo con Swing");
+    }
+}
